cae11e77638be78cf18cb8cdd598b3d60c921150
NaterPotater <nsro...@gmail.com>
2012-09-03 13:12:13 -0600

    Simple refactor of BoardGraph.
    - moved BoardGraph into Hasl namespace
      (remove boardGraph.js)
    
    - rely on nodeArray2d to create edges

--- js/boardSetup.js
+++ js/boardSetup.js
@@ -1,10 +1,81 @@
 var boardHeightInHexes = 6;
 var alphas = new Array('A','B','C','D','E','F','G','H','I');
 
+// TODO: move these globals into an interface...
 var clickedHexs = new Array();
 var selectionGroup;
 
 var Hasl = {};
+Hasl.BoardGraph = function() 
+{
+    this.graph = new Graph(); // using Dracula Graph Library
+    this.nodeArray2d = new Array();
+    
+    // some made up data for the board rep.
+    var boardHeightInHexes = 6;
+    var alphas = new Array('A','B','C','D','E','F','G','H','I');
+    
+    this.graph.edgeFactory.build = function(source, target) 
+    {
+        var e = jQuery.extend(true, {}, this.template);
+        e.source = source;
+        e.target = target;
+        e.directed = false;
+        e.style.label = e.weight = 1; //Math.floor(Math.random() * 10) + 1;
+        return e;
+    }
+    
+    //    function createNodes() 
+    for(var alphaIndex=0; alphaIndex<alphas.length; alphaIndex++)
+    {
+        this.nodeArray2d[alphaIndex] = new Array();
+        for (var numIndex=1; numIndex<=boardHeightInHexes; numIndex++)
+        {
+            var newNode = this.graph.addNode(alphas[alphaIndex] + numIndex.toString());
+            this.nodeArray2d[alphaIndex][numIndex-1] = newNode;
+        }
+    }
+    
+    //    function createEdges() 
+    var alphaSpan = this.nodeArray2d.length;
+    var numSpan = this.nodeArray2d[0].length;
+    for(alphaIndex=0; alphaIndex < alphaSpan; alphaIndex++)
+    {
+        for (numIndex=1; numIndex <= numSpan; numIndex++)
+        {
+            var currentHexId = this.nodeArray2d[alphaIndex][numIndex];
+
+            var notOnBottomEdge = (numIndex+1 <= boardHeightInHexes);
+            if(notOnBottomEdge)
+            {
+                notOnBottomEdge = true;
+                var hexBelowCurrent = this.nodeArray2d[alphaIndex][numIndex+1];
+                this.graph.addEdge(currentHexId, hexBelowCurrent);
+            }
+            
+            var notOnRightmostColumn = ((alphaIndex+1) < alphas.length);
+            if(notOnRightmostColumn)
+            {
+                var hexNextColumnBelow = this.nodeArray2d[alphaIndex+1][numIndex];
+                this.graph.addEdge(currentHexId, hexNextColumnBelow);
+            
+                var isOddRow = ((alphaIndex+1) % 2 == 0);
+                if(isOddRow && notOnBottomEdge)
+                {   // [i+1,j+2]
+                    var hexNextColumnBelow = this.nodeArray2d[alphaIndex+1][numIndex+1];
+                    this.graph.addEdge(currentHexId, hexNextColumnBelow);
+                }
+                var notOnTopRow = (numIndex > 1);
+                if(!isOddRow && notOnTopRow)
+                {
+                    var hexNextColumnAbove = this.nodeArray2d[alphaIndex+1][numIndex-1];
+                    this.graph.addEdge(currentHexId, hexNextColumnAbove);
+                }
+            }
+        }
+    }
+};
+
 Hasl.BoardHex = function(config, graphNode, messageLayer) {
     Kinetic.Group.call(this, config);
     
@@ -101,12 +172,11 @@ Hasl.BoardHex = function(config, graphNode, messageLayer) {
             }
         }
         selectionLayer.draw();
-        //layer.draw();
+    //layer.draw();
     });
     this.add(hexagon);
     this.add(simpleText);
 };
-
 Hasl.BoardHex.prototype.resetHexDefaults = function(hex) {
     hex.setFill("white");
     hex.setStrokeWidth(4);
@@ -127,6 +197,7 @@ Hasl.BoardHex.prototype.createSelectionHex = function() {
 }
 Kinetic.Global.extend(Hasl.BoardHex, Kinetic.Group);
 
+// TODO: move this into a well defined interface
 function drawBoard(/*BoardGraph*/ boardGraph, layer, messageLayer)
 {
     selectionGroup = new Kinetic.Group();
@@ -150,112 +221,6 @@ function drawBoard(/*BoardGraph*/ boardGraph, layer, messageLayer)
                 x: 75, 
                 y:70
             }, boardGraph.nodeArray2d[i][j], messageLayer);
-            newHex.move(xOffset, yOffset);
-            layer.add(newHex);
-            yOffset += y*2;
-        }
-        xOffset += x;
-    }
-}
-
-function createBoardHex(hexId, messageLayer)
-{
-    var group = new Kinetic.Group({
-        x: 75, //stage.getWidth() / 2, //220,
-        y: 70, //stage.getHeight() / 2 //40,
-        name: "group"+hexId
-    });
-	        
-    var hexagon = new Kinetic.RegularPolygon({
-        x: 0, //stage.getWidth() / 2,
-        y: 0, //stage.getHeight() / 2,
-        sides: 6,
-        radius: 70,
-        fill: "white",
-        stroke: "black",
-        strokeWidth: 4,
-        rotationDeg: 90,
-        name: "hex"+hexId,
-        id: hexId
-    });    
-        
-    var simpleText = new Kinetic.Text({
-        x: -70,
-        y: -70+12+4, //hexagon.y + hexagon.radius,
-        text: hexId,
-        fontSize: 12,
-        fontFamily: "Calibri",
-        textFill: "black",
-        align: "center",
-        width: 140,
-        name: "text"+hexId
-    });
-
-    group.on('mouseout', function() { 
-        var layer = this.getLayer();
-        var hex = group.get("#"+hexId)[0];
-        resetHexDefaults(hex);
-        while(clickedHexs.length)
-        {
-            resetHexDefaults(clickedHexs.pop());
-        }
-            
-        layer.draw();
-        writeMessage(messageLayer, hexId + " <mouseout>"); 
-    });
-    group.on('mousemove', function() {
-        var layer = this.getLayer();
-        var hex = group.get("#"+hexId)[0];
-        hex.setStrokeWidth(4);
-        hex.setFill("yellow");
-        this.moveToTop();
-        hex.setStrokeWidth(6);
-        layer.draw();
-        //        var mousePos = stage.getMousePosition();
-        //        var x = mousePos.x - 190;
-        //        var y = mousePos.y - 40;
-        writeMessage(messageLayer, hexId + " <hover>");
-    });
-    group.on("mousedown touchstart", function() 
-    {
-        // do a distance calc to all other hexes
-        writeMessage(messageLayer, hexId + " <click>"); 
-    //        var node = findNode(hexId);
-    //        for(var i=0; i < node.edges.length; i++)
-    //        {
-    //            var adjacentHex = group.get("#"+(node.edges[i].id))[0];
-    //            adjacentHex.setFill("pink");
-    //            clickedHexs.push(adjacentHex);
-    //        }
-    //        layer.draw();
-    });
-        
-    group.add(hexagon);
-    group.add(simpleText);
-
-    return group;
-}
-
-function createBoard(layer, selectionLayer, messageLayer)
-{
-    var y = 60;
-    var x = 105;
-    var yOffset = 0;
-    var xOffset = 0;
-    for(var i=0; i<alphas.length; i++)
-    {
-
-        yOffset = 0;
-        if((i+1) % 2 == 0) 
-        {
-            yOffset += y;
-        }
-        for (var j=1; j<=boardHeightInHexes; j++)
-        {
-            var hexId = alphas[i] + j.toString();
-            var newHex = createBoardHex(hexId, selectionLayer, messageLayer);
-				 
-				
             newHex.move(xOffset, yOffset);
             layer.add(newHex);
             yOffset += y*2;
